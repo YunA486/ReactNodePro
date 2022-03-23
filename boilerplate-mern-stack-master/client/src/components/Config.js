291e108378b2337f7d1807cad29253b4e9df8587
YunA486 <w2039@e-mirim.hs.kr>
2022-03-23 23:48:28 +0900

    Movie Main

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -2,5 +2,5 @@
 export const USER_SERVER = '/api/users';
 
 export const API_URL = 'https://api.themoviedb.org/3/';
-
+export const API_KEY = '2f13d2fab1fec7a66e7379f63a9127c4';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
