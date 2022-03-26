8d5227f0e0e214db177ce47ca292977e8af09b81
YunA486 <w2039@e-mirim.hs.kr>
2022-03-26 21:22:18 +0900

    FavoritePage..

--- boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
+++ boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './favorite.css';
+import Axios from 'axios';
 
 function FavoritePage() {
+
+    const [Favorites, setFavorites] = useState([])
+
+    useEffect(() => {
+
+        Axios.post('/api/favorite/getFavoredMovie', {userFrom: localStorage.getItem('userId')})
+            .then(response => {
+                if(response.data.success){
+                    setFavorites(response.data.Favorites)
+                } else {
+                    alert('영화 정보를 가져오는데 실패 했습니다.')
+                }
+            })
+
+    }, [])
+
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
         <h2> Favorite Movies </h2>
@@ -16,7 +33,13 @@ function FavoritePage() {
                 </tr>
             </thead>
             <tbody>
-
+                {Favorites.map((favorite, index) => (
+                    <tr key={index}>
+                        <td>{favorite.movieTitle}</td>
+                        <td>{favorite.movieRunTime} mins</td>
+                        <td><button>Remove</button></td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     </div>
