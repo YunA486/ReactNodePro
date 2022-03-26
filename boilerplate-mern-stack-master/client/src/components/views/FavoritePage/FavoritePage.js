8e65f646e8b402d352fa80fa85fd42bd4143ef2a
YunA486 <w2039@e-mirim.hs.kr>
2022-03-26 22:57:46 +0900

    Delete Button

--- boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
+++ boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import './favorite.css';
 import Axios from 'axios';
+import { Popover } from 'antd';
+import { IMAGE_BASE_URL } from '../../Config';
 
 function FavoritePage() {
 
@@ -8,16 +10,58 @@ function FavoritePage() {
 
     useEffect(() => {
 
-        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId')})
-            .then(response => {
-                if(response.data.success){
-                    setFavorites(response.data.favorites)
-                } else {
-                    alert('영화 정보를 가져오는데 실패 했습니다.')
-                }
-            })
+        fetchFavoredMovie()
 
     }, [])
+
+    const fetchFavoredMovie = () => {
+        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId')})
+        .then(response => {
+            if(response.data.success){
+                setFavorites(response.data.favorites)
+            } else {
+                alert('영화 정보를 가져오는데 실패 했습니다.')
+            }
+        })
+    }
+
+    const onClickDelete = (movieId, userFrom) => {
+
+        const variables = {
+            movieId,
+            userFrom
+        }
+
+        Axios.post('/api/favorite/removeFromFavorite', variables)
+            .then(response => {
+                if (response.data.success){
+                    fetchFavoredMovie()
+                } else {
+                    alert("리스트에서 지우는데 실패했습니다.")
+                }
+            })
+    }
+
+    const renderCards = Favorites.map((favorite, index) => {
+        
+        const content = (
+            <div>
+                {favorite.moviePost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"}
+                {/* } */}
+            </div>
+        )
+
+        return <tr key={index}>
+
+            <Popover content={content} title={`${favorite.movieTitle}`}>
+                <td>{favorite.movieTitle}</td>
+            </Popover>
+
+            <td>{favorite.movieRunTime} mins</td>
+            <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+        </tr>
+    })
 
   return (
     <div style={{ width: '85%', margin: '3rem auto' }}>
@@ -33,13 +77,7 @@ function FavoritePage() {
                 </tr>
             </thead>
             <tbody>
-                {Favorites.map((favorite, index) => (
-                    <tr key={index}>
-                        <td>{favorite.movieTitle}</td>
-                        <td>{favorite.movieRunTime} mins</td>
-                        <td><button>Remove</button></td>
-                    </tr>
-                ))}
+                {renderCards}
             </tbody>
         </table>
     </div>
